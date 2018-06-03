d9feb0dde8b115518eb9dde74bdcd6c5cce2f99c
Navdeep Singh <navdeep.er@gmail.com>
2018-06-03 14:50:35 +0530

    Progress Save for Homepage

--- js/main.js
+++ js/main.js
@@ -140,25 +140,31 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.setAttribute('alt', restaurant.name);
+  image.setAttribute('title', restaurant.name);
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const listWrapper = document.createElement('div');
+  listWrapper.setAttribute('class', 'list-info');
+  li.append(listWrapper);
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  listWrapper.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  listWrapper.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  listWrapper.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  listWrapper.append(more);
 
   return li
 }
