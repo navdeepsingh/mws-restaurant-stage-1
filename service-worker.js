fc1e7a088fa5b135694366473f78cb5ec00d708e
Navdeep Singh <navdeep.er@gmail.com>
2018-08-20 11:35:17 +0530

    SW optimized

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,15 @@
 (function() {
   'use strict';
 
-  const staticCacheName = 'cacheFile-mws-stage1-v1.7';
+  const staticCacheName = 'cacheFile-mws-stage1-v2.0';
   const imageCacheName = 'images';
+  const pagesCacheName = 'pages';
+
+  const cacheList = [
+    staticCacheName,
+    imageCacheName,
+    pagesCacheName
+  ];
   const cacheFiles = [
     '/',
     '/restaurant.html?id=',
@@ -15,6 +22,7 @@
   self.addEventListener('install', installEvent => {
     console.log('Service Worker Installing');
 
+    skipWaiting();
     installEvent.waitUntil(
       caches.open(staticCacheName)
         .then( staticCache => {
@@ -31,12 +39,15 @@
         .then( cacheNames => {
           return Promise.all(
             cacheNames.map( cacheName => {
-              if (cacheName != staticCacheName) {
-                return caches.delete(cacheName); // Delete the old cache
+              if (!cacheList.includes(cacheName)) {
+                return caches.delete(cacheName);
               }
             })
           )
-        }),
+        })
+        .then( () => {
+          return clients.claim();
+        })
     );
   });
 
@@ -44,31 +55,54 @@
     console.log('Service Worker is Listening');
     const request = fetchEvent.request;
 
-      fetchEvent.respondWith(        
+    // When the user requests an HTML file
+    // if (request.headers.get('Accept').includes('text/html')) {
+    //   fetchEvent.respondWith(
+    //     // Fetch that page from the network
+    //     fetch(request)
+    //   ); // end respondWith
+    //   return; // Go no further
+    // }
+
+    // When the user requests an image
+    if (request.url.match(/\.(jpe?g|png|gif|svg)$/)) {
+      fetchEvent.respondWith(
         caches.match(request)
-        .then( responseFromCache => {
+          .then(responseFromCache => {
+            if (responseFromCache) {
+              return responseFromCache;
+            }
+
+            // Otherwise fetch from network         
+            return fetch(request)
+              .then(responseFromFetch => {
+                // Special cache for images
+                const copy = responseFromFetch.clone();
+                fetchEvent.waitUntil(
+                  caches.open(imageCacheName)
+                    .then(imageCache => {
+                      return imageCache.put(request, copy);
+                    })
+                );
+                return responseFromFetch;
+              });
+          })
+      ); // end respondWith
+      return; // Go no further
+    } 
+
+    // For everything else...
+    fetchEvent.respondWith(
+      // Look for a cached version of the file
+      caches.match(request)
+        .then(responseFromCache => {
           if (responseFromCache) {
             return responseFromCache;
-          }
-
-          // Otherwise fetch from network         
-          return fetch(request)
-          .then(responseFromFetch => {
-            // Special cache for images
-            if (request.headers.get('Accept').includes('image')) {
-              const copy = responseFromFetch.clone();
-              fetchEvent.waitUntil(
-                caches.open(imageCacheName)
-                .then(imageCache => {
-                  return imageCache.put(request, copy);
-                })
-              );
-            }
-            return responseFromFetch;            
-
-          });
-        })
-      ); // end respondWith
+          } // end if
+          
+          return fetch(request);      
+        })        
+    ); // end respondWith
   });
 
   
